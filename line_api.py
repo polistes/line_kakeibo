65d96afa3c644b5fa6f34c09a90eb8b6b9419a37
polistes <polistes19@gmail.com>
2020-01-11 16:30:59 +0100

    use group member profile

--- line_api.py
+++ line_api.py
@@ -14,8 +14,9 @@ class LineAPI():
   def __init__(self):
     with open('conf/line.json') as f:
       self.line_conf = json.load(f)
-      self.logger = logging.getLogger('flask.app')
-      self.id2name = {}
+
+    self.logger = logging.getLogger('flask.app')
+    self.id2name = {}
 
   def validate_segnature(self, request):
     # https://developers.line.biz/ja/docs/messaging-api/building-bot/#spy-anchor-e0e7507abac7b64bdaa502657638da62bab4190e
@@ -40,31 +41,40 @@ class LineAPI():
     return True
 
   def is_text_message(self, event):
-    if event['type'] == 'message':
-      if event['message']['type'] == 'text':
-        return True
-    return False
+    return event['type'] == 'message' and event['message']['type'] == 'text'
 
-  def get_display_name(self, user_id):
+  def get_display_name(self, line_event):
     # https://developers.line.biz/ja/reference/messaging-api/#anchor-0190762129db8ef085b86fff02f55c97027211f2
-    if user_id in self.id2name:
-      return self.id2name[user_id]
-    else:
-      url = 'https://api.line.me/v2/bot/profile/{}'.format(user_id)
-      headers = {'Authorization': 'Bearer {}'.format(self.line_conf['access_token'])}
-      result = requests.get(url, headers = headers)
-      profile = json.loads(result.text)
-      if 'displayName' not in profile:
-        self.logger.warning('key \'displayName\' not found in profile: {}'.format(result.text))
-        return user_id
+    #
+    if 'source' not in line_event:
+      self.logger.warning('key \'source\' not found in line_event: {}'.format(line_event))
+      # TODO どうしよう
+      return 'null'
+    if 'userId' not in line_event['source'] or \
+        'groupId' not in line_event['source']:
+      self.logger.warning('key \'userId\' or \'groupId\' not found in source of line_event: {}'.format(line_event['source']))
+      # TODO どうしよう
+      return 'null'
+    user_id = line_event['source']['userId']
+    group_id = line_event['source']['groupId']
 
-      self.id2name[user_id] = profile['displayName']
-      if result.status_code == 200:
-        self.logger.info('successfully get user profile {} -> {}'.format(user_id, profile['displayName']))
-        return profile['displayName']
-      else:
-        self.logger.warning('failed to get user profile of {} with status code: {}'.format(user_id, result.status_code))
-        return user_id
+    self.logger.info('groupId: {}, userId: {}'.format(group_id, user_id))
+
+    url = 'https://api.line.me/v2/bot/group/{}/member/{}'.format(group_id, user_id)
+    headers = {'Authorization': 'Bearer {}'.format(self.line_conf['access_token'])}
+    result = requests.get(url, headers = headers)
+    profile = json.loads(result.text)
+    if 'displayName' not in profile:
+      self.logger.warning('key \'displayName\' not found in profile: {}'.format(result.text))
+      return user_id
+
+    self.id2name[user_id] = profile['displayName']
+    if result.status_code == 200:
+      self.logger.info('displayName found for userId: {} -> {}'.format(user_id, profile['displayName']))
+      return profile['displayName']
+    else:
+      self.logger.warning('failed to get user profile of {} with status code: {}'.format(user_id, result.status_code))
+      return user_id
 
   def send_replay(self, event, response_body):
     response = {}
--- webhook.py
+++ webhook.py
@@ -11,6 +11,7 @@ class Webhook():
 
   def __init__(self):
     self.line_api = LineAPI()
+    self.logger = logging.getLogger('flask.app')
     self.spreadsheet = SpreadSheet()
 
   def post(self, request):
@@ -20,17 +21,17 @@ class Webhook():
     line_event = msg['events'][0]
 
     if self.line_api.is_text_message(line_event):
-      display_name = self.line_api.get_display_name(line_event['source']['userId'])
+      display_name = self.line_api.get_display_name(line_event)
 
       message = line_event['message']['text']
-      logging.info("user: %s sent message: %s" % (display_name, message))
+      self.logger.info("user: %s sent message: %s" % (display_name, message))
 
       return_msg = self.spreadsheet.append(display_name, message)
 
       self.line_api.send_replay(line_event, return_msg)
 
     else:
-      logging.info("ignored non text message")
-      logging.info("  `-- event type: %s" % line_event['type'])
+      self.logger.info("ignored non text message")
+      self.logger.info("  `-- event type: %s" % line_event['type'])
       if line_event['type'] == 'message':
-        logging.info("  `-- message type: %s" % line_event['message']['type'])
+        self.logger.info("  `-- message type: %s" % line_event['message']['type'])
