77d419b44dd60c0ee9a6ab18a0f93628b0206d0a
polistes <polistes19@gmail.com>
2020-03-19 18:19:04 +0100

    add is_today method

--- utils.py
+++ utils.py
@@ -5,3 +5,9 @@ from datetime import datetime, timedelta, timezone
 def get_current_time():
   JST = timezone(timedelta(hours=+9), 'JST')
   return datetime.now(JST)
+
+def is_today(date_str):
+  now = get_current_time()
+  current_date = datetime(now.year, now.month, now.day)
+  record_date = datetime.strptime(date_str, '%Y/%m/%d')
+  return record_date == current_date
