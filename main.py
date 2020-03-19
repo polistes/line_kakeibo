b273e2307422f0a2745dd01406068b0b8339d448
polistes <polistes19@gmail.com>
2020-03-19 16:00:36 +0100

    enable logging

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
+#-*- coding: utf-8 -*-
+
 from flask import Flask, request
+
+import google.cloud.logging
 
 from webhook import Webhook
 from spreadsheet import SpreadSheet
+
+# ログ出力設定
+# https://cloud.google.com/logging/docs/setup/python#connecting_the_library_to_python_logging
+# https://qiita.com/yoko8ma/items/bfa108596d5025501705
+client = google.cloud.logging.Client()
+client.setup_logging()
 
 app = Flask(__name__)
 mywebhook = Webhook()
